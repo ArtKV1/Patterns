2f41d01ce7a1454216019e8e27e03b21308fb9ab
ArtKV <89652798+ArtKV1@users.noreply.github.com>
2024-10-08 11:35:46 +0300

    Lab 1. Task 9. Add method setContacts if at least one contact does not exist this

--- lab1/Student.kt
+++ lab1/Student.kt
@@ -41,6 +41,7 @@ data class Student(
         return id == other.id
     }
 
+
     companion object {
         fun isValidPhone(phone: String?): Boolean {
             return phone?.matches(Regex("""^\+7\d{1,10}${'$'}""")) ?: false
@@ -57,11 +58,13 @@ data class Student(
 
 
         fun validate(gitHub: String?, phone: String?, telegram: String?, email: String?): Boolean {
+
             if (gitHub == null) {
                 throw IllegalArgumentException("Требуется гитхаб")
             }
             if (phone == null && telegram == null && email == null) {
-                throw IllegalArgumentException("Требуется хотя бы один контакт (номер телефона, телеграм или почта)")
+                println("Требуется хотя бы один контакт (номер телефона, телеграм или почта)")
+                println("Дальше введите контактные данные")
             }
             return true
         }
@@ -72,6 +75,11 @@ data class Student(
 
         if (validate(gitHub, phone, telegram, email))
         {
+            if (phone == null && email == null && telegram == null)
+            {
+                setContacts()
+            }
+
             if (phone != null && !isValidPhone(phone)) {
                 throw IllegalArgumentException("Неверный номер телефона: $phone")
             }
@@ -85,5 +93,14 @@ data class Student(
                 throw IllegalArgumentException("Неверный гитхаб: $gitHub")
             }
         }
+    }
+
+    fun setContacts(newPhone: String? = readln(), newTelegram: String? = readln(), newEmail: String? = readln()) {
+        if (newPhone != null && newPhone != "")
+            phone = newPhone
+        if (newTelegram != null && newPhone != "")
+            telegram = newTelegram
+        if (newEmail != null && newEmail != "")
+            email = newEmail
     }
 }
