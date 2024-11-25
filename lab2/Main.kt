1c808eb28934121caa4ed21a144405c8a725457d
ArtKV <89652798+ArtKV1@users.noreply.github.com>
2024-11-25 19:01:16 +0300

    Lab 2. Task 3. Добавление извлечения краткой информации о студенте

--- lab2/Main.kt
+++ lab2/Main.kt
@@ -2,11 +2,11 @@ package org.example
 
 import Student
 
-fun main() 
-{
+fun main() {
     val stud = Student("Lermontov","Mihail","Yurevich", emailValue = "literature@gmail.com", gitHubValue = "famous007", phoneNumberValue = "+79186916942");
     println(stud)
     val data = Student.parseString("Student(id:1,surname:Lermontov,name:Mihail,patronymic:Yurevich,phoneNumber:+79186916942,email:literature@gmail.com,telegram:,gitHub:famous007)")
     println(data)
     println(Student(data))
+    println(stud.getInfo())
 }
