ad4534516de335fb36185fc20ddbdb708c45300a
ArtKV <89652798+ArtKV1@users.noreply.github.com>
2024-11-25 19:03:13 +0300

    Lab 2. Task 4. Добавление класса StudentShort

--- lab2/Main.kt
+++ lab2/Main.kt
@@ -1,12 +1,16 @@
 package org.example
 
 import Student
+import StudentShort
 
 fun main() {
     val stud = Student("Lermontov","Mihail","Yurevich", emailValue = "literature@gmail.com", gitHubValue = "famous007", phoneNumberValue = "+79186916942");
-    println(stud)
-    val data = Student.parseString("Student(id:1,surname:Lermontov,name:Mihail,patronymic:Yurevich,phoneNumber:+79186916942,email:literature@gmail.com,telegram:,gitHub:famous007)")
+    val data = Student.parseString("Student(id:4,surname:Lermontov,name:Mihail,patronymic:Yurevich,phoneNumber:+79186916942,email:literature@gmail.com,telegram:,gitHub:famous007)")
     println(data)
+    println(stud)
     println(Student(data))
+    println(Student(Student.parseString("Student(id:1,surname:Romanov,name:Vladislav,patronymic:Vitalevich,phoneNumber:+78005553535,email:romadis@gmail.com,telegram:,gitHub:romadis)")))
     println(stud.getInfo())
+    println(StudentShort(4,stud.getInfo()))
+    println(StudentShort(stud))
 }
