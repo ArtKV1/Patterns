f013b605eb292108432bb1dceb53f954a7079042
ArtKV <89652798+ArtKV1@users.noreply.github.com>
2024-11-25 19:05:44 +0300

    Lab 2. Task 5-6. Рефактор кода и добавление диаграммы классов

--- lab2/Main.kt
+++ lab2/Main.kt
@@ -4,13 +4,25 @@ import Student
 import StudentShort
 
 fun main() {
+    val students = mutableListOf<Student>();
+    students.add(Student("Mamin-Sibiryak","Dmitriy","Narkisovich", telegramValue = "@poet777"));
+    students.add(Student("Mahmudov","Ararat","Sarkisovich", gitHubValue = "abu123"));
+    students.add(Student("Lermontov","Mihail","Yurevich", emailValue = "literature@gmail.com"));
+    students.add(Student("Chernikov","Alexander","Viktorovich", phoneNumberValue = "+79095889812"));
+    students.forEach { it: Student -> println(it) };
+    println(Student("Lermontov","Mihail","Yurevich", emailValue = "literature@gmail.com", gitHubValue = "famous007").validate());
+
     val stud = Student("Lermontov","Mihail","Yurevich", emailValue = "literature@gmail.com", gitHubValue = "famous007", phoneNumberValue = "+79186916942");
+    stud.setContacts(hashMapOf(Pair("email","literature@gmail.com"),Pair("telegram","@miha999"),Pair("gitHub",null)));
+    println(stud)
+    stud.name = "Mishanya";
+
     val data = Student.parseString("Student(id:4,surname:Lermontov,name:Mihail,patronymic:Yurevich,phoneNumber:+79186916942,email:literature@gmail.com,telegram:,gitHub:famous007)")
     println(data)
     println(stud)
     println(Student(data))
-    println(Student(Student.parseString("Student(id:1,surname:Romanov,name:Vladislav,patronymic:Vitalevich,phoneNumber:+78005553535,email:romadis@gmail.com,telegram:,gitHub:romadis)")))
+    println(Student(Student.parseString("Student(id:3,surname:Chernenko,name:Viktor,patronymic:Grigorevich,gitHub:)")))
     println(stud.getInfo())
-    println(StudentShort(4,stud.getInfo()))
+    println(StudentShort(4,Student("Lermontov","Mihail","Yurevich", gitHubValue = "famous007", telegramValue = "@poetishka").getInfo()))
     println(StudentShort(stud))
 }
