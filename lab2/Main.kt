2fed06f19b645b31781c85ed17763f051a8d8eef
ArtKV <89652798+ArtKV1@users.noreply.github.com>
2024-11-25 19:08:32 +0300

    Lab 2. Task 7. Добавление метода readFromTxt и txt файл со студентами

--- lab2/Main.kt
+++ lab2/Main.kt
@@ -1,7 +1,5 @@
 package org.example
-
 import Student
-import StudentShort
 
 fun main() {
     val students = mutableListOf<Student>();
@@ -9,20 +7,8 @@ fun main() {
     students.add(Student("Mahmudov","Ararat","Sarkisovich", gitHubValue = "abu123"));
     students.add(Student("Lermontov","Mihail","Yurevich", emailValue = "literature@gmail.com"));
     students.add(Student("Chernikov","Alexander","Viktorovich", phoneNumberValue = "+79095889812"));
+    students.add(Student(hashMapOf(Pair("name","Artur"),Pair("surname","Kharisov"),Pair("patronymic","Marselevich"))));
     students.forEach { it: Student -> println(it) };
-    println(Student("Lermontov","Mihail","Yurevich", emailValue = "literature@gmail.com", gitHubValue = "famous007").validate());
 
-    val stud = Student("Lermontov","Mihail","Yurevich", emailValue = "literature@gmail.com", gitHubValue = "famous007", phoneNumberValue = "+79186916942");
-    stud.setContacts(hashMapOf(Pair("email","literature@gmail.com"),Pair("telegram","@miha999"),Pair("gitHub",null)));
-    println(stud)
-    stud.name = "Mishanya";
-
-    val data = Student.parseString("Student(id:4,surname:Lermontov,name:Mihail,patronymic:Yurevich,phoneNumber:+79186916942,email:literature@gmail.com,telegram:,gitHub:famous007)")
-    println(data)
-    println(stud)
-    println(Student(data))
-    println(Student(Student.parseString("Student(id:3,surname:Chernenko,name:Viktor,patronymic:Grigorevich,gitHub:)")))
-    println(stud.getInfo())
-    println(StudentShort(4,Student("Lermontov","Mihail","Yurevich", gitHubValue = "famous007", telegramValue = "@poetishka").getInfo()))
-    println(StudentShort(stud))
+    println(Student.readFromTxt("src/test.txt").forEach { println(it) })
 }
